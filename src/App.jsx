aca81ad69562b35b5f8687884f3498d4b85e43a5
HarishBattarasetty <Battarasettyharish@gmail.com>
2023-03-06 14:10:51 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem vero voluptas, accusantium dolorem culpa neque sapiente optio dolore eligendi ipsa.
+    </div>
+  )
+}
+
+export default App
